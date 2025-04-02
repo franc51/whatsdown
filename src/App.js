3e1ce1fb296be069e5822482a9e553d71e0a8eab
Francisc Szasz <101558212+franc51@users.noreply.github.com>
2025-04-02 20:40:07 +0300

    aaa

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import WelcomePage from "./Components/Welcome-page/welcome-page.jsx";
-import Login from "./Components/Login/login.jsx";
 import Homepage from "./Components/Homepage/homepage.jsx";
 
 function App() {
