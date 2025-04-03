edc400cd2cab7be60fe148a8b58e9a723ebf59f3
Francisc Szasz <101558212+franc51@users.noreply.github.com>
2025-04-03 11:57:33 +0300

    finished design login

--- src/Components/Login/login.jsx
+++ src/Components/Login/login.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import "./login.css"
 
 export default function Login() {
-  return <p>login works!</p>;
+  return <div>
+          <p>Log In into your account.</p>
+          <div className="login_container">
+            <div className="login_container">
+              <label>E-mail</label>
+              <input type="email" placeholder="something@company.com" required></input>
+            </div>
+            <div className="login_container">
+              <label>Password</label>
+              <input type="password" placeholder="your super strong password" required></input>
+              <a className="login_forgotPassword" href="blank.html">Forgot your password?</a>
+            </div>
+            <button className="login_button_submit">Log In</button>
+          </div>
+        </div>;
 }
